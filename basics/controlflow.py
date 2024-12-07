12f1ca6906d7a9bc6a124e566d02b05b6e6de914
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-12-07 12:08:31 +0530

    line continuation

---
+++ basics/controlflow.py
@@ -0,0 +1,5 @@
+age = 37
+if age == 37:
+    print("True")
+else:
+    print("False")
---
+++ basics/line_continuation.py
@@ -0,0 +1,4 @@
+# Illustrating line continuation using backslash(\)
+sum_expr = 1 + 2 + 3 + 4 \
+           + 5 + 6
+print(sum_expr)
