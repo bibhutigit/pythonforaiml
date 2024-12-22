81a6bece1998f577f5711b22ed5d4a51147067b2
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2024-12-22 19:44:10 +0530

    generators using yield

--- generators/generatorsexample.py
+++ generators/generatorsexample.py
@@ -15,3 +15,26 @@ def my_num_cube_generator(n):
 for n in my_num_cube_generator(5):
     print(n)
 
+def gen_fibo(n):
+    a = 1
+    b = 1
+
+    for num in range(n):
+        yield a
+        a, b = b, a+b
+
+print("Fibonacci Series")
+for num in gen_fibo(10):
+    print(num)
+
+#iter() and next() demonstration
+
+def int_val(num):
+    for i in range(num):
+        yield i
+
+my_int_itr = int_val(10)
+
+print(next(my_int_itr)) # Prints 0
+print(next(my_int_itr)) # Prints 1
+print(next(my_int_itr)) # # Prints 2
